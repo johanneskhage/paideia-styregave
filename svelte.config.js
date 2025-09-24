cf250c64ebfc063c9246a40d8fe5b8123860c66f
johanneskhage <johannesk.hage@gmail.com>
2025-09-24 14:13:44 +0200

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      out: 'build',
-      precompress: true
-    }),
+    adapter: adapter(),
     alias: {
       $lib: './src/lib'
     }
@@ -15,4 +12,4 @@ const config = {
   preprocess: vitePreprocess()
 };
 
-export default config; 
+export default config;
